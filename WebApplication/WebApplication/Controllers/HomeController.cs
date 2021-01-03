cc8fb03fa9333265d26a736918ed07eceb091342
cly4auHo <arsen.dmitriev96@gmail.com>
2021-01-03 09:55:19 +0200

    init

---
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
