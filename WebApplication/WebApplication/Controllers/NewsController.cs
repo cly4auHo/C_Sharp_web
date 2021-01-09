fd531eca7ed59b6dd7c2a1927f4ada93d08d890b
cly4auHo <arsen.dmitriev96@gmail.com>
2021-01-09 02:10:00 +0200

    init

---
+++ WebApplication/WebApplication/Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.News = NewsBase.News;
+            return View();
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/News.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsFake { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Title: {1}, Text:{2}, Author:{3}, Is Fake: {4} ", Id, Title, Text, AuthorName, IsFake);
+        }
+    }
+}
